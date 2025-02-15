a76c136c04ba945c19c9fb9cab8b0019f476830b
Brono25 <bronston.a@icloud.com>
2025-02-16 04:36:29 +1100

    formatting

--- Core/Src/mpm.c
+++ Core/Src/mpm.c
@@ -1,14 +1,9 @@
-
-
 /*
- *
  *  McLeod Pitch Method
  *  A smarter way to find pitch: https://www.cs.otago.ac.nz/students/postgrads/tartini/papers/A_Smarter_Way_to_Find_Pitch.pdf
  *
  *  Uses a normalised auto correlation and parabolic intperpolation for increased accuracy.
 */
-
-
 
 #include <stdio.h>
 #include <string.h>
@@ -16,22 +11,26 @@
 #include <math.h>
 #include "mpm.h"
 
-
-#define PEAK_THRESHOLD 0.9
-
-float32_t mpm_reserved_memory[2 * BLOCK_SIZE] = {0};
-
-
-
-void print_arr(float *arr, uint16_t length)
+void mpm_mcleod_pitch_method_f32(float32_t *pData, float32_t *pitch_estimate)
 {
-	for (uint16_t i = 0; i < length; i++)
-	{
-		printf("%f \n", arr[i]);
-	}
-	printf("end\n");
-}
+	float32_t *p_ncorr;
+	mpm_NSDF_f32(pData, &p_ncorr);
+	uint16_t tau = 1;
+	mpm_find_peak_f32(p_ncorr, &tau);
 
+	if (tau > BLOCK_SIZE - 2)
+		tau = BLOCK_SIZE - 2;
+
+	uint16_t xp = tau;
+	float32_t a = p_ncorr[tau - 1];
+	float32_t b = p_ncorr[tau];
+	float32_t c = p_ncorr[tau + 1];
+
+	float32_t delta_tau = 0;
+	mpm_parabolic_interpolation_f32(xp, a, b, c, &delta_tau);
+
+	*pitch_estimate = FS / delta_tau;
+}
 
 void mpm_sum_f32(float32_t *pSrc, uint16_t scrLen, float32_t *pRes)
 {
@@ -43,47 +42,36 @@ void mpm_sum_f32(float32_t *pSrc, uint16_t scrLen, float32_t *pRes)
 	}
 }
 
-
 void mpm_find_peak_f32(float32_t *pSrc, uint16_t *tau)
 {
-	uint16_t flag = 0;
-	uint16_t valid_peak_flag = 0;
+	uint16_t flag = FIRST_CROSSING_NOT_PASSED;
+	uint16_t valid_peak_flag = VALID_PEAK_NOT_FOUND;
 	float32_t peak_value = 0;
 
 	for (uint16_t i = 0; i < BLOCK_SIZE; i++)
     {
-
-       if (flag == 0 && *pSrc < 0)
-       {
-           flag = 1;
-
-       }
-       if (flag == 1)
+       if (flag == FIRST_CROSSING_NOT_PASSED && *pSrc < 0)
+           flag = FIRST_CROSSING_PASSED;
+       
+       if (flag == FIRST_CROSSING_PASSED)
        {
        		if (*pSrc > peak_value && *pSrc > PEAK_THRESHOLD)
        		{
 				peak_value = *pSrc;
               	*tau = i;
-                valid_peak_flag = 1;
+                valid_peak_flag = VALID_PEAK_FOUND;
 
-       		} else if (valid_peak_flag == 1)
-       		{
+       		} else if (valid_peak_flag == VALID_PEAK_FOUND)
        			return;
-       		}
        }
        pSrc++;
     }
 }
 
-
 void mpm_NSDF_f32(float32_t *pSrc, float32_t **pDst)
 {
-
 	float32_t *xcorr = &mpm_reserved_memory[1];
-
-
 	arm_correlate_f32(&pSrc[0], BLOCK_SIZE , &pSrc[0], BLOCK_SIZE, xcorr);
-
 
 	float32_t *r = &xcorr[BLOCK_SIZE - 1];
 	*pDst = r;
@@ -97,10 +85,8 @@ void mpm_NSDF_f32(float32_t *pSrc, float32_t **pDst)
 	mpm_sum_f32(&xs[0], BLOCK_SIZE, &xs1);
 	xs2 = xs1;
 
-
 	for (uint16_t tau = 0; tau < BLOCK_SIZE  ; tau++)
 	{
-
 		*r = 2 * (*r) / (xs1 + xs2);
 
 		xs1 = xs1 - (*p_xs1);
@@ -112,46 +98,13 @@ void mpm_NSDF_f32(float32_t *pSrc, float32_t **pDst)
 	}
 }
 
-
 void mpm_parabolic_interpolation_f32(uint16_t x_pos, float32_t a, float32_t b, float32_t c, float32_t *delta_tau)
 {
 	a = 20*log10(a);
 	b = 20*log10(b);
 	c = 20*log10(c);
-
 	float32_t delta_pos = 0.5 * (a - c) / (1 - 2.0*b + c);
-
 	*delta_tau = x_pos + delta_pos;
-}
-
-
-void mpm_mcleod_pitch_method_f32(float32_t *pData, float32_t *pitch_estimate)
-{
-
-
-	float32_t *p_ncorr;
-
-	mpm_NSDF_f32(pData, &p_ncorr);
-	uint16_t tau = 1;
-	mpm_find_peak_f32(p_ncorr, &tau);
-
-
-
-   if (tau > BLOCK_SIZE - 2)
-   {
-   	tau = BLOCK_SIZE - 2;
-   }
-
-	uint16_t xp = tau;
-	float32_t a = p_ncorr[tau - 1];
-	float32_t b = p_ncorr[tau];
-	float32_t c = p_ncorr[tau + 1];
-
-	float32_t delta_tau = 0;
-	mpm_parabolic_interpolation_f32(xp, a, b, c, &delta_tau);
-
-
-	*pitch_estimate = FS / delta_tau;
 }
 
 
